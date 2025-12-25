2e84b499292b0ddc2311cc800a5d71903c73ee85
Gusle01 <qkrdudwn010@gmail.com>
2025-12-26 08:14:43 +0900

    Project4 initial commit (frontend + backend)

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
-
 import { useAuthStore } from "../stores/useAuthStore";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://113.198.66.75:13127",
-  //baseURL: "http://localhost:3000",
-  withCredentials: true,
+  baseURL: "/api",          // ⭐ 핵심
+  //baseURL: "http://113.198.66.75:13127",
+  withCredentials: true,    // refreshToken 쿠키
 });
 
+// 요청 인터셉터 (accessToken 자동 첨부)
 axiosInstance.interceptors.request.use((config) => {
   const accessToken = useAuthStore.getState().accessToken;
+
   if (accessToken) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
+
   return config;
 });
